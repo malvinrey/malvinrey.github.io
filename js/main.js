15ec928419716a50294bf5b8c74975ff26cd0ad1
malvinrey <malvinreynarajawakory@gmail.com>
2024-10-17 08:16:21 +0700

    terakhir

--- js/main.js
+++ js/main.js
@@ -9,11 +9,17 @@
             }
         }, 1);
     };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+    spinner(0);
+
+
+    // Fixed Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-200px');
+        }
+    });
     
     
    // Back to top button
@@ -30,45 +36,19 @@
     });
 
 
-    // Team carousel
-    $(".team-carousel").owlCarousel({
+    // Latest-news-carousel
+    $(".latest-news-carousel").owlCarousel({
         autoplay: true,
-        smartSpeed: 1000,
+        smartSpeed: 2000,
         center: false,
-        dots: false,
+        dots: true,
         loop: true,
-        margin: 50,
+        margin: 25,
         nav : true,
         navText : [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
-
-    // Testimonial carousel
-
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: true,
-        dots: true,
-        loop: true,
-        margin: 0,
-        nav : true,
-        navText: false,
         responsiveClass: true,
         responsive: {
             0:{
@@ -82,25 +62,64 @@
             },
             992:{
                 items:3
+            },
+            1200:{
+                items:4
             }
         }
     });
 
 
-     // Fact Counter
+    // What's New carousel
+    $(".whats-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:2
+            }
+        }
+    });
 
-     $(document).ready(function(){
-        $('.counter-value').each(function(){
-            $(this).prop('Counter',0).animate({
-                Counter: $(this).text()
-            },{
-                duration: 2000,
-                easing: 'easeInQuad',
-                step: function (now){
-                    $(this).text(Math.ceil(now));
-                }
-            });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
         });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
     });
 
 
